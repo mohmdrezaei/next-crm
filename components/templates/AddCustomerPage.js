fdd83619fd63fb0befef241151d9b62d1bf82537
mohmdrezaei <mhwmdrz9@gmail.com>
2024-11-25 14:07:38 +0330

    Create Add Customer page

---
+++ components/templates/AddCustomerPage.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Form from "../modules/Form";
+
+function AddCustomerPage() {
+  const [form, setForm] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    address: "",
+    postalCode: "",
+    date: "",
+    products: [],
+  });
+  const saveHandler = () => {};
+  const cancelHandler = () => {};
+  return (
+    <div className="customer-page">
+      <h4>Add New Customer</h4>
+      <Form form={form} setForm={setForm} />
+      <div className="customer-page__buttons">
+        <button className="first" onClick={cancelHandler}>
+          Cancel
+        </button>
+        <button className="second" onClick={saveHandler}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default AddCustomerPage;
