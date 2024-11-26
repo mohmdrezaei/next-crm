073d15c8c91a565fc95d1be0c4d68e7cba9a6faf
mohmdrezaei <mhwmdrz9@gmail.com>
2024-11-26 21:20:35 +0330

    Creating a home page as an SSR and connecting to the database

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
-export default function Home() {
-  return (
-    <h1>next-crm</h1>
-  )
+import connectDB from "../utils/connectDB";
+import Customer from "../models/Customer";
+import HomePage from "../components/templates/HomePage";
+
+function Index({ customers }) {
+  console.log(customers)
+  return <HomePage customers={customers} />;
+}
+export default Index;
+
+export async function getServerSideProps() {
+  try {
+    await connectDB();
+    const customers = await Customer.find();
+    return {
+      props: {
+        customers: JSON.parse(JSON.stringify(customers)),
+      },
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
 }
