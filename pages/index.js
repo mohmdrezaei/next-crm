cc0c7bae2c89cceb39f963f1aaa1245c26c706d7
mohmdrezaei <mhwmdrz9@gmail.com>
2024-11-26 22:16:11 +0330

    Show customers on the home page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Customer from "../models/Customer";
 import HomePage from "../components/templates/HomePage";
 
 function Index({ customers }) {
-  console.log(customers)
   return <HomePage customers={customers} />;
 }
 export default Index;
