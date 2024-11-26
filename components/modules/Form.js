014d563a965f229af76ba57e4aafe3572f459c21
mohmdrezaei <mhwmdrz9@gmail.com>
2024-11-26 20:31:37 +0330

    Create form Component

--- components/modules/Form.js
+++ components/modules/Form.js
@@ -1,9 +1,65 @@
-import ItemList from "./ItemList"
+import FormInput from "./FormInput";
+import ItemList from "./ItemList";
 
-function Form({form , setForm}) {
+function Form({ form, setForm }) {
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setForm({...form, [name] : value })
+  };
   return (
-    <div><ItemList form={form} setForm={setForm} /></div>
-  )
+    <div>
+      <FormInput
+        name="name"
+        label="name"
+        type="text"
+        value={form.name}
+        onChange={changeHandler}
+      />
+      <FormInput
+        name="lastName"
+        label="Last Name"
+        type="text"
+        value={form.lastName}
+        onChange={changeHandler}
+      />
+      <FormInput
+        name="email"
+        label="Email"
+        type="text"
+        value={form.email}
+        onChange={changeHandler}
+      />
+      <FormInput
+        name="phone"
+        label="Phone"
+        type="tel"
+        value={form.phone}
+        onChange={changeHandler}
+      />
+      <FormInput
+        name="address"
+        label="Address"
+        type="text"
+        value={form.address}
+        onChange={changeHandler}
+      />
+      <FormInput
+        name="postalCode"
+        label="Postal Code"
+        type="text"
+        value={form.postalCode}
+        onChange={changeHandler}
+      />
+      <FormInput
+        name="date"
+        label="Date"
+        type="date"
+        value={form.date}
+        onChange={changeHandler}
+      />
+      <ItemList form={form} setForm={setForm} />
+    </div>
+  );
 }
 
-export default Form
+export default Form;
